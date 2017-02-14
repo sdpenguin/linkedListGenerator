5fda850c2f8d206438883474d9ebe2150b6e3885
unknown <wbe15@eews304-001.ic.ac.uk>
2017-02-14 11:00:50 +0000

    Got the linkedlist program working - need to now output the values to test it and then delete the list at the end (deallocate the memory)

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,22 +1,41 @@
-#include <stdio>
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <windows.h>
 
 using namespace std;
 
-struct linkedList
+struct linkedListElement
 {
 	int data;
-	linkedList* nextItem; //This will be null if at end of list
+	linkedListElement* nextElement; //This will be null if at end of list
 };
 
 int main()
 {
-	linkedList list;
-	for(int i=0, j=0; i<45; i++,j++)
+	string option = "restart";
+	cout << "Welcome to linkedList - designed by Waleed El-Geresy\n";
+	do
 	{
-		5.6
-
+		if(option=="restart")
+		{
+			linkedListElement* nextElementPointer = NULL; //We create a pointer that will be used as the end of the list
+			int n,el;
+			cout << "How many elements would you like this LL to have? ";
+			cin >> n;
+			for(int i=0, j=0; i<n; i++,j++) //Less than n because the list is indexed from 0 to n-1 (n elements)
+			{
+				linkedListElement* l = new linkedListElement;
+				l->nextElement = nextElementPointer; //Initialised to NULL, and then repetitively set to the current element
+				cout << "Data for element " << i+1 << ": ";
+				cin >> el; //TEST this to see whether you can simply replace el with l->data?
+				l->data = el;
+				nextElementPointer = l;
+			}
+		}
+		cout << "Please enter 'exit' to continue, 'restart' to go again: ";
+		cin >> option;
 	}
+	while(option!="exit");
+	cout << "Thank you for using this program!";
 }
