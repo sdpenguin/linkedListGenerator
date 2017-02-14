5830796b69b9b3221b96ec80952ecf7ae407becc
unknown <wbe15@eews304-001.ic.ac.uk>
2017-02-14 10:19:34 +0000

    First commit

---
+++ linkedList.cpp
@@ -0,0 +1,22 @@
+#include <stdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct linkedList
+{
+	int data;
+	linkedList* nextItem; //This will be null if at end of list
+};
+
+int main()
+{
+	linkedList list;
+	for(int i=0, j=0; i<45; i++,j++)
+	{
+		5.6
+
+	}
+}
